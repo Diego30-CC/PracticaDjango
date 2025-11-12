ecabd54b2861ff3345fdfc50345b206db526d0d2
Diego30-CC <dacccastillo5272@gmail.com>
2025-11-12 09:12:01 -0500

    modifica

--- misitio/settings.py
+++ misitio/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = ['*']
 
 # CSRF_TRUSTED_ORIGINS = ['https://*']
 CSRF_TRUSTED_ORIGINS = [
-    os.getenv("DOMINIO" ,"http://127.0.0.1:8000" )
+    os.getenv("DOMINIO" ,"http://127.0.0.1:8000/")
     ]
 
 
